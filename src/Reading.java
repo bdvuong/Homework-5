2455b7cdbfd6a4ec5b897dd4eaad7b866de2b8cd
vuongdbrandon <57996169+vuongdbrandon@users.noreply.github.com>
2020-11-30 20:33:51 -0500

    reading class

---
+++ src/Reading.java
@@ -0,0 +1,14 @@
+/**
+ * A reading from the sensor.
+ * Contains:
+ *  time of the reading
+ *  temperature at the time of reading
+ *  amount of rainfall at the time of reading
+ */
+public class Reading {
+
+    Time time;
+    double temperature;
+    double rainFall;
+
+}
